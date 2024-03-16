db11b2674593a2c27a6d3625776378f41cb93f6f
Joseph DALY <joseph300905@gmail.com>
2024-03-16 19:05:03 +0100

    new minecraft server

--- systemd/minecraft/default.nix
+++ systemd/minecraft/default.nix
@@ -4,7 +4,7 @@
   wants = [ "network-online.target" ];
 
   serviceConfig = {
-    ExecStart = "${pkgs.jdk}/bin/java -Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui";
+    ExecStart = "${pkgs.jre8}/bin/java -server -Xmx12000M -Xms12000M -jar Server.jar -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy";
     Restart = "always";
     User = "mcuser";
     WorkingDirectory = "/home/mcuser";
